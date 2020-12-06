5ab6dae3703c6fc3db8daaa864af1596e3edade7
unknown <you@example.com>
2020-12-06 19:43:14 +0530

    initial setup

---
+++ world-atlas/src/components/Layout/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import styles from "./Layout.module.css";
+
+const Layout = ({children}) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>World Atlas</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        {children}
+      </main>
+
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  );
+};
+
+
+export default Layout;
---
+++ world-atlas/src/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Layout from '../components/Layout/Layout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return 
+  <Layout>
+    Main
+  </Layout>
+  
+}
