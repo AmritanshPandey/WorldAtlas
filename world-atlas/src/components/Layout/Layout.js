8b3d3da305b18a7185d2095ed060c88def43e359
unknown <you@example.com>
2020-12-11 22:12:07 +0530

    Countries Table Added

--- world-atlas/src/components/Layout/Layout.js
+++ world-atlas/src/components/Layout/Layout.js
@@ -1,19 +1,28 @@
 import Head from "next/head";
 import styles from "./Layout.module.css";
 
-const Layout = ({children}) => {
+
+
+const Layout = ({children, title = "World Atlas"}) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>World Atlas</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+
+     <header className={styles.header}>
+      <span className={styles.logo}>WORLD ATLAS</span>
+     </header>
 
       <main className={styles.main}>
         {children}
       </main>
 
-      <footer className={styles.footer}>Footer</footer>
+      <footer className={styles.footer}>
+      Copyright &copy; Amritansh Pandey
+      </footer>
     </div>
   );
 };
--- world-atlas/src/pages/index.js
+++ world-atlas/src/pages/index.js
@@ -1,11 +1,32 @@
 import Head from 'next/head'
 import Layout from '../components/Layout/Layout'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import SearchInput from "../components/SearchInput/SearchInput";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 
-export default function Home() {
-  return 
-  <Layout>
-    Main
-  </Layout>
+
+export default function Home({ countries }) {
+
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} Countries </div>
+      <SearchInput placeholder="Search By Name, Region or Sub-region" />
+
+      <CountriesTable countries={countries} />
+    </Layout>
+  );
   
 }
+
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
