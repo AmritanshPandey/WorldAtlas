db5cd7f6911935a1880bd0918721b272531b833d
unknown <you@example.com>
2020-12-26 23:51:59 +0530

    search integrated

--- world-atlas/src/pages/index.js
+++ world-atlas/src/pages/index.js
@@ -1,28 +1,40 @@
-import Head from 'next/head'
-import Layout from '../components/Layout/Layout'
+import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
-
+import { useState } from "react";
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword)
+  );
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
 
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} Countries </div>
-      <SearchInput placeholder="Search By Name, Region or Sub-region" />
+      <SearchInput
+        placeholder="Search By Name, Region or Sub-region"
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
-  
 }
-
 
 export const getStaticProps = async () => {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
   const countries = await res.json();
-
 
   return {
     props: {
